25114fd5de9dfe69b45b41b86b95d90db7d61926
Stanislav Krupin <gladislav4@gmail.com>
2024-11-26 14:25:14 +0200

    1.1

--- js/main.js
+++ js/main.js
@@ -1,53 +1,7 @@
-let swiper = new Swiper(".gallery-swiper", {
-    slidesPerView: 1,
-    spaceBetween: 15,
-    pagination: {
-        el: ".gallery-pagination",
-        type: "fraction"
-    },
-    navigation: {
-        nextEl: ".gallery__btn-next",
-        prevEl: ".gallery__btn-prev"
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 15,
-        },
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 15,
-        },
-        1024: {
-            slidesPerView: 4,
-            spaceBetween: 15,
-        },
-    },
-});
+const burgerMenu = document.getElementById("burgerMenu");
+const mobileMenu = document.getElementById("mobileMenu");
 
-let swiper2 = new Swiper(".gallery-swiper2", {
-    slidesPerView: 1,
-    spaceBetween: 15,
-    pagination: {
-        el: ".gallery-pagination2",
-        type: "fraction"
-    },
-    navigation: {
-        nextEl: ".gallery__btn-next2",
-        prevEl: ".gallery__btn-prev2"
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 15,
-        },
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 15,
-        },
-        1024: {
-            slidesPerView: 4,
-            spaceBetween: 15,
-        },
-    },
+burgerMenu.addEventListener("click", () => {
+  mobileMenu.style.display =
+    mobileMenu.style.display === "block" ? "none" : "block";
 });

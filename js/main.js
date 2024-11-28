a64a4092d90e9ea68c4f57a4b8de7b4403f02d39
Stanislav Krupin <gladislav4@gmail.com>
2024-11-28 12:23:02 +0200

    1.2

--- js/main.js
+++ js/main.js
@@ -5,3 +5,29 @@ burgerMenu.addEventListener("click", () => {
   mobileMenu.style.display =
     mobileMenu.style.display === "block" ? "none" : "block";
 });
+
+// Получаем элементы
+const modal = document.getElementById("modal");
+const modalImage = document.getElementById("modalImage");
+const closeModal = document.querySelector(".close");
+const photos = document.querySelectorAll(".photo");
+
+// Открытие модального окна
+photos.forEach((photo) => {
+  photo.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImage.src = photo.src; // Устанавливаем выбранное фото в модалку
+  });
+});
+
+// Закрытие модального окна
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Закрытие модального окна при клике вне изображения
+modal.addEventListener("click", (e) => {
+  if (e.target !== modalImage) {
+    modal.style.display = "none";
+  }
+});
